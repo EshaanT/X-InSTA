6caa2b5de0da18ac0406c967f05bcaecce81a071
EshaanT <78814815+EshaanT@users.noreply.github.com>
2023-11-02 21:05:19 +0530

    Update dataset_creation.py

--- dataset_creation.py
+++ dataset_creation.py
@@ -5,10 +5,3 @@ from utils.data import create_few_shots
 
 create_few_shots(dataset_name='amaz_bi',k=4,seeds=[32,5,232,100,42],set_up='src_is_cross')
 create_few_shots(dataset_name='amaz_bi',k=4,seeds=[32],set_up='sim_in_cross') #No Need to create 5 seeds as here demos will be same in all seeds (Nearest neighbor always same)
-    
-create_few_shots(dataset_name='xnli',k=4,seeds=[32,5,232,100,42],set_up='src_is_cross')
-create_few_shots(dataset_name='xnli',k=4,seeds=[32],set_up='sim_in_cross')#No Need to create 5 seeds as here demos will be same in all seeds (Nearest neighbor always same)
-    
-create_few_shots(dataset_name='xnli',k=4,seeds=[32,5,232,100,42],set_up='src_is_cross')
-create_few_shots(dataset_name='xnli',k=4,seeds=[32],set_up='sim_in_cross')#No Need to create 5 seeds as here demos will be same in all seeds (Nearest neighbor always same)
-
